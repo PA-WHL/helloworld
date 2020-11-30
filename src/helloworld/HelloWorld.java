0bd2ba5643cfd7c6c35dbf174542bb6a51473436
WHL <WHL@DESKTOP-Q8EGKNB>
2020-11-30 16:19:22 +0800

    解决中文问题

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("hello world!");
-		System.out.println("Ëæ±ãÔö¼ÓÒ»µãĞŞ¸Ä£¡");
+		System.out.println("éšä¾¿å¢åŠ ä¸€ç‚¹ä¿®æ”¹");
 	}
 }
